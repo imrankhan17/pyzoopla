1a7b5d894c98b3a354daae5e172cf6802700924a
Khan, Imran, Vodafone Group <imran.khan18@vodafone.com>
2018-06-11 16:14:56 +0100

    minor changes to setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,31 @@
 import setuptools
 
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open('README.md', 'r') as f:
+    long_description = f.read()
 
 setuptools.setup(
-    name="pyzoopla",
-    version="0.1",
-    author="Imran Khan",
-    author_email="imrankhan17@hotmail.co.uk",
-    description="A Python package to access information about properties from Zoopla",
+    name='pyzoopla',
+    version='0.1.1',
+    author='Imran Khan',
+    author_email='imrankhan17@hotmail.co.uk',
+    description='A Python package to access information about properties from Zoopla',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/imrankhan17/properties",
+    long_description_content_type='text/markdown',
+    url='https://github.com/imrankhan17/properties',
     packages=setuptools.find_packages(),
+    license='MIT',
+    install_requires=[
+        'beautifulsoup4',
+        'bs4',
+        'lxml',
+        'numpy',
+        'pandas',
+        'requests'
+    ],
     classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ),
 )
